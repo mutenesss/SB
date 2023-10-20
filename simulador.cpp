be2c469d953d95f066c76ab4c5f0045077f96466
mutenesss <erickfraga0301@gmail.com>
2023-10-20 13:39:41 -0300

    simulador

---
+++ simulador.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	// Verifica se apenas um arquivo foi passado como argumento
+	if(argc > 2)
+	{
+		cout << "Erro no input de arquivo." << endl;
+		return 1;
+	}
+
+	string line;
+	fstream file(argv[1], ios::in);
+	vector<int> program;
+	int num;
+	
+	if(file.is_open())
+	{
+		// Leitura do arquivo e armazenamento no vetor program
+		while (getline(file, line))
+		{
+			stringstream iss (line);
+			while (iss >> num)
+			{
+				program.push_back(num);
+			}
+		}
+		file.close();
+	}
+	else
+	{
+		cout << "Erro na abertura de arquivo";
+		return 1;
+	}
+
+	for(auto i : program)
+	{
+		cout << i << endl;
+	}
+}
