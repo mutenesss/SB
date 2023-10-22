495d08693a99fe6f6f7e612afb33dd4ba5313841
mutenesss <erickfraga0301@gmail.com>
2023-10-22 11:57:11 -0300

    simulador

--- readme.md
+++ readme.md
@@ -38,10 +38,17 @@ Bibliotecas Necessárias
 Bibliotecas padrão C++ e Bibliotecas Boost 
 ```
 
+_Como Utilizar_
+```plaintext
+./montador arquivo.asm
+Ambos os arquivos precisam estar na mesma pasta
+```
+
 Montador do assembly inventado
 
 *   Não possui macro.
 *   Arquivos de entrada .asm **não precisam** terminar com enter.
+*	Apenas 1 arquivo .asm pode ser passado por vez.
 *   Não é permitido o uso de SPACE ou CONST sem label.
 *   Não é necessária a existência de secao data separada.
 *   Ocorre error caso não tenha secao text.
@@ -51,4 +58,33 @@ Montador do assembly inventado
 
 **\- simulador.cpp**
 
-**404**
+_Como Compilar_
+
+```plaintext
+g++ -Wall -o simulador simulador.cpp
+```
+
+Padrão C++ utilizado
+
+```plaintext
+C++20
+```
+
+Bibliotecas Necessárias
+
+```plaintext
+Bibliotecas padrão C++
+```
+
+_Como Utilizar_
+```plaintext
+./simulador arquivo.obj
+Ambos os arquivos precisam estar na mesma pasta
+```
+
+Simulador do assembly inventado
+
+*	Tem como entrada os arquivos .obj obtidos do montador.
+*   Simulação iterativa do programa.
+*   Em casos de invasão da área de memória, o programa encerra diretamente.
+*   Apenas 1 arquivo .obj pode ser passado por vez.
--- simulador.cpp
+++ simulador.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include <boost/algorithm/string.hpp>
+
 using namespace std;
 
 /*Alunos
@@ -16,42 +16,29 @@ C++
 
 int main(int argc, char *argv[]) {
 
-	map<int, int>Opcode;
-
-	//Adding the elements
-	Opcode[1] = 2;
-	Opcode[2] = 2;
-	Opcode[3] = 2;
-	Opcode[4] = 2;
-	Opcode[5] = 2;
-	Opcode[6] = 2;
-	Opcode[7] = 2;
-	Opcode[8] = 2;
-	Opcode[9] = 3;
-	Opcode[10] = 2;
-	Opcode[11] = 2;
-	Opcode[12] = 2;
-	Opcode[13] = 2;
-	Opcode[14] = 1;
-
 	string f_type = ".obj";
 
+	//Verifica se o arquivo foi adicionado no argumento
 	if(argc != 2){
 		cout << "Erro no input de arquivo." << endl;
 		return -1;
 	}
 
+	//Transforma o nome do arquivo em uma string
 	string f_name = argv[1];
 
+	//Verifica se o arquivo é .obj
     if (f_name.find(f_type) == string::npos) {
 		cout << "Erro de extenção de arquivo." << endl;
 		return -1;
 	}
 
+	//Abre o arquivo
 	ifstream f_obj;
   	f_obj.open (f_name, ios::in);
 	vector<int> v_data;
 
+	//Armazena o texto do arquivo em um vetor
 	if (f_obj) {        
 		int value; 
 		while ( f_obj >> value ) {
@@ -59,23 +46,29 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	//Fecha o arquivo
 	f_obj.close();
-	//int pc = 0;
-	int e_data = int(v_data.size());
 
-	for(int j = 0; j < int(v_data.size()); j++){
-		for (auto op :Opcode){
-			if (op.first == v_data[j]) {
-				for (int i = 0; i < op.second - 1; i++) {
-					j++;
-					if((v_data[j] < e_data)&&((op.first > 8)||(op.first <5))&&(v_data[j] > 0)) e_data = v_data[j];
-				}
+	int e_data = int(v_data.size());
+	int j = 0;
+	vector<int> op = {-1,2,2,2,2,2,2,2,2,3,2,2,2,2,1};
+
+	//Loop usado para descobrir o menor endereço de dados que foram chamados no programa
+	//O programa não identifica um endereço caso o mnemonico que chamou o endereçor for um jump
+	while(j < int(v_data.size())){
+		int aux_1 = v_data[j];
+
+		for (int i = 0; i < op[aux_1] - 1; i++) {
+			j += 1;
+			if((v_data[j] < e_data)&&((aux_1 > 8)||(aux_1 <5))&&(v_data[j] > 0)) {
+				e_data = v_data[j];
 			}
-		}
+			}
+		j += 1;
 	}
 
+	//Parte principal do program, que identificaos comandos e mostra o que foi pedido
 	int acc = 0;
-
 	for(int pc = 0; pc < int(v_data.size()); pc += 0){
 		switch (v_data[pc]) {
 			case 1:
@@ -109,45 +102,53 @@ int main(int argc, char *argv[]) {
 			case 5:
 				cout<<"JMP"<<endl;
 				pc += 1;
-				pc = v_data[v_data[pc]];
+				pc = v_data[pc];
 				cout<<"PC <- "<<pc<<" ACC <- "<<acc<<endl;
-				if (e_data >= pc){
+				//Este if é usado para identificar se o jump vai para a região de memoria de dados
+				if (e_data <= pc){
 					cout<<"SEGMENTATION FAULT"<<endl;
 					return -1;
 				}
+				//getchar();
 				break;
 			case 6:
 				cout<<"JMPN"<<endl;
 				pc += 1;
-				if(acc < 0) pc = v_data[v_data[pc]];
+				if(acc < 0) pc = v_data[pc];
 				else pc += 1;
 				cout<<"PC <- "<<pc<<" ACC <- "<<acc<<endl;
-				if (e_data >= pc){
+				//Este if é usado para identificar se o jump vai para a região de memoria de dados
+				if (e_data <= pc){
 					cout<<"SEGMENTATION FAULT"<<endl;
 					return -1;
 				}
+				//getchar();
 				break;
 			case 7:
 				cout<<"JMPP"<<endl;
 				pc += 1;
-				if(acc > 0) pc = v_data[v_data[pc]];
+				if(acc > 0) pc = v_data[pc];
 				else pc += 1;
 				cout<<"PC <- "<<pc<<" ACC <- "<<acc<<endl;
-				if (e_data >= pc){
+				//Este if é usado para identificar se o jump vai para a região de memoria de dados
+				if (e_data <= pc){
 					cout<<"SEGMENTATION FAULT"<<endl;
 					return -1;
 				}
+				//getchar();
 				break;
 			case 8:
 				cout<<"JMPZ"<<endl;
 				pc += 1;
-				if(acc == 0) pc = v_data[v_data[pc]];
+				if(acc == 0) pc = v_data[pc];
 				else pc += 1;
 				cout<<"PC <- "<<pc<<" ACC <- "<<acc<<endl;
-				if (e_data >= pc){
+				//Este if é usado para identificar se o jump vai para a região de memoria de dados
+				if (e_data <= pc){
 					cout<<"SEGMENTATION FAULT"<<endl;
 					return -1;
 				}
+				//getchar();
 				break;
 			case 9:
 				cout<<"COPY"<<endl;
